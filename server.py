558b23adff37a658cd651f333ea26e93fde5ef62
Azeem Haque <91793824+azeemhaque@users.noreply.github.com>
2023-09-03 00:41:58 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import util
+app = Flask(__name__)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations' : util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    sqft = float(request.form['sqft'])
+    site_location = request.form['site_location']
+    BHK = int(request.form['BHK'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price' : util.get_estimated_price(site_location,sqft,BHK,bath)
+    })
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+if  __name__ == "__main__":
+    print("Starting Python Flask Server for Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
